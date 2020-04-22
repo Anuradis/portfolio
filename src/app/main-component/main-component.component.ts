e89738732e60bd7b2479b98181c81e75d4d3cc12
Anuradis <p.pachocki@gmail.com>
2020-04-22 10:19:17 +0100

    portfolio items toggle functionality completed

--- src/app/main-component/main-component.component.ts
+++ src/app/main-component/main-component.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { SlideInOutAnimation } from '../../assets/animations/animations';
-import { clearLine } from 'readline';
 
 @Component({
   selector: 'app-main-component',
@@ -12,18 +11,27 @@ import { clearLine } from 'readline';
 
 export class MainComponentComponent implements OnInit {
 
-  weatherAppContent = false;
-  fetchServerContent = false;
-  toDoContentContent = false;
-  animationStateWeatherApp = 'out';
-  animationStateFetchServer = 'out';
-  animationStateToDo = 'out';
-  windowTop = true;
+  windowTop = false;
   scrollOn = true;
   scrollPositionY = 0;
   showMore = true;
-  showMoreName = 'Show More';
+
+  weatherAppContent = false;
+  ampContent = false;
+  toDoContentContent = false;
+  fetchServerContent = false;
+  playballContent = false;
+  webDesignContent = false;
+
+  animationStateWeatherApp = 'out';
+  animationStateFetchServer = 'out';
+  animationStateToDo = 'out';
   animationStateShowMore = 'out';
+  animationStateAmp = 'out';
+  animationStatePlayball = 'out';
+  animationStateWebDesign = 'out';
+
+  showMoreName = 'Show More';
 
   @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
     if (Math.floor(window.scrollY) > 700) {
@@ -35,40 +43,67 @@ export class MainComponentComponent implements OnInit {
     }
 }
 
-onScroll(el: HTMLElement) {
-  if (this.scrollPositionY > 700) {
-    window.scrollTo(0, 0);
-    } else if (this.scrollPositionY <= 700) {
-      el.scrollIntoView();
-     }
-}
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  toggleContentWeatherApp($event): void {
+  onScroll(el: Element) {
+    if (this.scrollPositionY > 700) {
+      window.scrollTo(0, 0);
+      } else if (this.scrollPositionY <= 700) {
+        el.scrollIntoView();
+       }
+  }
+
+  onToggleContentWeatherApp($event, target: Element): void {
       this.animationStateWeatherApp = this.animationStateWeatherApp === 'out' ? 'in' : 'out';
       this.weatherAppContent = !this.weatherAppContent;
       $event.preventDefault();
+      target.scrollIntoView();
   }
 
-  toggleContentFetchServer($event) {
-
-    this.animationStateFetchServer = this.animationStateFetchServer === 'out' ? 'in' : 'out';
-    this.fetchServerContent = !this.fetchServerContent;
+  onToggleContentAmp($event, el: Element) {
+    this.animationStateAmp = this.animationStateAmp === 'out' ? 'in' : 'out';
+    this.ampContent = !this.ampContent;
     $event.preventDefault();
+    el.scrollIntoView();
   }
 
-  toggleContentToDo($event) {
+  onToggleContentToDo($event, el: Element) {
     this.animationStateToDo = this.animationStateToDo === 'out' ? 'in' : 'out';
     this.toDoContentContent = !this.toDoContentContent;
     $event.preventDefault();
+    el.scrollIntoView();
   }
-  onShowMore(el: HTMLElement, $event) {
+
+  onToggleContentFetchServer($event, el: Element) {
+    this.animationStateFetchServer = this.animationStateFetchServer === 'out' ? 'in' : 'out';
+    this.fetchServerContent = !this.fetchServerContent;
+    $event.preventDefault();
+    el.scrollIntoView();
+  }
+
+  onToggleContentPlayball($event, el: Element) {
+    this.animationStatePlayball = this.animationStatePlayball === 'out' ? 'in' : 'out';
+    this.playballContent = !this.playballContent;
+    $event.preventDefault();
+    el.scrollIntoView();
+  }
+
+  onToggleContentWebDesign($event, el: Element) {
+    this.animationStateWebDesign = this.animationStateWebDesign === 'out' ? 'in' : 'out';
+    this.webDesignContent = !this.webDesignContent;
+    $event.preventDefault();
+    el.scrollIntoView();
+  }
+
+
+
+  onShowMore($event, el: Element) {
       if (this.animationStateShowMore) {
         this.showMoreName = 'Show More';
-        el.scrollIntoView();
        } else if (!this.animationStateShowMore) {
       this.showMoreName = 'Show Less';
       $event.preventDefault();
