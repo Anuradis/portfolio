d6f02bac43c148a3d3fdc791b18973c1b06fed62
Anuradis <p.pachocki@gmail.com>
2020-05-01 09:54:20 +0100

    onScroll method refactored, my-projects template ref passed to main-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,25 @@ import { NavComponentComponent } from './nav-component/nav-component.component';
 import { MainComponentComponent } from './main-component/main-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { ResumeComponent } from './main-component/resume/resume.component';
+import { AboutComponent } from './main-component/resume/about/about.component';
+import { ExperienceComponent } from './main-component/resume/experience/experience.component';
+import { SkillsComponent } from './main-component/resume/skills/skills.component';
+import { MyProjectsComponent } from './main-component/my-projects/my-projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponentComponent,
     MainComponentComponent,
-    FooterComponentComponent
+    ResumeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    MyProjectsComponent,
+    FooterComponentComponent,
+
+
   ],
   imports: [
     BrowserModule,
--- src/app/main-component/my-projects/my-projects.component.ts
+++ src/app/main-component/my-projects/my-projects.component.ts
@@ -1,15 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, Output, EventEmitter } from '@angular/core';
+import { SlideInOutAnimation } from 'src/assets/animations/animations';
 
 @Component({
   selector: 'app-my-projects',
   templateUrl: './my-projects.component.html',
-  styleUrls: ['./my-projects.component.css']
+  styleUrls: ['./my-projects.component.css'],
+  animations: [SlideInOutAnimation]
 })
-export class MyProjectsComponent implements OnInit {
+
+
+export class MyProjectsComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('seeMore', {static: false}) someFiled: ElementRef;
+  @Output() sendSeeMoreElement = new EventEmitter<ElementRef>();
+
+  weaterAppImg: string = '../../assets/images/weather_app.png';
+  weaterAppImgDraft: string = '../../assets/images/weather_app-min.png';
+  apmImg: string = '../../assets/images/apm.png';
+  apmImgDraft: string = '../../assets/images/apm-min.png';
+  todoListImg: string = '../../assets/images/todo_list.png';
+  todoListImgDraft: string = '../../assets/images/todo_list-min.png';
+  fetchNewsImg: string = '../../assets/images/fetch_news-server.png';
+  fetchNewsImgDraft: string = '../../assets/images/fetch_news-server-min.png';
+  playballImg: string = '../../assets/images/playball_game.png';
+  playballImgDraft: string = '../../assets/images/playball_game-min.png';
+  webDesignImg: string = '../../assets/images/web-design.png';
+  webDesignImgDraft: string = '../../assets/images/web-design-min.png';
+
+
+  weatherAppContent = false;
+  ampContent = false;
+  toDoContentContent = false;
+  fetchServerContent = false;
+  playballContent = false;
+  webDesignContent = false;
+
+  animationStateWeatherApp = 'out';
+  animationStateFetchServer = 'out';
+  animationStateToDo = 'out';
+  animationStateShowMore = 'out';
+  animationStateAmp = 'out';
+  animationStatePlayball = 'out';
+  animationStateWebDesign = 'out';
+
+  showMore = true;
+  showMoreName = 'See More';
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.sendSeeMoreElement.emit(this.someFiled);
   }
+
+  onToggleContentWeatherApp($event, target: Element): void {
+    this.animationStateWeatherApp = this.animationStateWeatherApp === 'out' ? 'in' : 'out';
+    this.weatherAppContent = !this.weatherAppContent;
+    $event.preventDefault();
+    target.scrollIntoView();
+}
+
+onToggleContentAmp($event, el: Element): void {
+  this.animationStateAmp = this.animationStateAmp === 'out' ? 'in' : 'out';
+  this.ampContent = !this.ampContent;
+  $event.preventDefault();
+  el.scrollIntoView();
+}
+
+onToggleContentToDo($event, el: Element): void {
+  this.animationStateToDo = this.animationStateToDo === 'out' ? 'in' : 'out';
+  this.toDoContentContent = !this.toDoContentContent;
+  $event.preventDefault();
+  el.scrollIntoView();
+}
+
+onToggleContentFetchServer($event, el: Element): void {
+  this.animationStateFetchServer = this.animationStateFetchServer === 'out' ? 'in' : 'out';
+  this.fetchServerContent = !this.fetchServerContent;
+  $event.preventDefault();
+  el.scrollIntoView();
+}
+
+onToggleContentPlayball($event, el: Element): void {
+  this.animationStatePlayball = this.animationStatePlayball === 'out' ? 'in' : 'out';
+  this.playballContent = !this.playballContent;
+  $event.preventDefault();
+  el.scrollIntoView();
+}
+
+onToggleContentWebDesign($event, el: Element): void {
+  this.animationStateWebDesign = this.animationStateWebDesign === 'out' ? 'in' : 'out';
+  this.webDesignContent = !this.webDesignContent;
+  $event.preventDefault();
+  el.scrollIntoView();
+}
+
+onShowMore(event): void {
+
+  this.animationStateShowMore = this.animationStateShowMore === 'out' ? 'in' : 'out';
+  if (this.animationStateShowMore === 'out') {
+      this.showMoreName = 'See More';
+     } else {
+    this.showMoreName = 'See Less';
+    event.preventDefault();
+  }
+}
 
 }
